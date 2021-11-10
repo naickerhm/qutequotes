da219ae6b1e7f556726524cac5916075a74e83ce
hnaicker <hnaicker@student.wethinkcode.co.za>
2021-11-10 20:30:25 +0200

    WIP database

--- server/src/main/java/DataAccess/DatabaseConnection.java
+++ server/src/main/java/DataAccess/DatabaseConnection.java
@@ -1,2 +1,59 @@
-package DataAccess;public class DatabaseConnection {
+package DataAccess;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DatabaseConnection {
+
+    // public static final String DISK_DB_URL = "jdbc:sqlite:QuteQuotes.sqlite";
+    public static final String DISK_DB_URL = "jdbc:sqlite::memory";
+
+    public  Connection connectionObj = null;
+
+
+    public DatabaseConnection() {
+        try( final Connection connection = DriverManager.getConnection(DISK_DB_URL) ){
+            System.out.println( "Connected to database " );
+            connectionObj = connection;
+        }catch( SQLException e ){
+            System.err.println( e.getMessage() );
+        }
+    }
+
+//    public Map<Integer, Quote> retrieveQuotes() {
+//        return null;
+//
+//    }
+
+    public void createTable() throws SQLException {
+        String sqlQuery =  "CREATE TABLE IF NOT EXISTS QuteQuotesList (quote TEXT, author TEXT)";
+        try( final Statement stmt = connectionObj.createStatement() ){
+            stmt.execute(sqlQuery);
+        }catch( SQLException e ){
+            System.err.println( e.getMessage() );
+        }
+    }
+
+
+
+    private void addQuote() throws SQLException { //incomplete
+        String sqlQuery =  "INSERT INTO QuteQuotesList (quote, author) VALUES ()";
+        try( final Statement stmt = connectionObj.createStatement() ){
+            stmt.execute(sqlQuery);
+        }catch( SQLException e ){
+            System.err.println( e.getMessage() );
+        }
+    }
+
+    private void readQuotes () throws SQLException { //TODO
+        String sqlQuery =  "SELECT Author FROM QuteQuotesList";
+        try( final Statement stmt = connectionObj.createStatement() ){
+            stmt.execute(sqlQuery);
+        }catch( SQLException e ){
+            System.err.println( e.getMessage() );
+        }
+    }
+
 }
--- server/src/main/java/DataAccess/NewQuteQuotes.java
+++ server/src/main/java/DataAccess/NewQuteQuotes.java
@@ -1,2 +1,43 @@
-package DataAccess;public class NewQuteQuotes {
+package DataAccess;
+
+import java.sql.*;
+
+
+public class NewQuteQuotes {
+    public void saveNewQuotes(String authorName, String quoteText, String DISK_DB_URL)
+            throws SQLException
+    {
+        Connection con = DriverManager.getConnection(DISK_DB_URL);
+        PreparedStatement restmt = con.prepareStatement(
+                "SELECT * FROM author WHERE author.authorName = ?"
+        );
+        restmt.setString(1, authorName);
+        restmt.execute();
+        ResultSet results = restmt.getResultSet();
+        if(results.next()){
+            con.close();
+            throw new RuntimeException("World name must be unique");
+        }
+
+        try( final PreparedStatement stmt = con.prepareStatement(
+                "INSERT INTO author(authorName, quoteText) VALUES (?, ?)") ){
+            stmt.setString(1, authorName);
+            stmt.setString(2, quoteText);
+            boolean gotAResultSet = stmt.execute();
+            if(gotAResultSet){
+                throw new RuntimeException( "Unexpectedly got a SQL resultset." );
+            }else{
+                final int updateCount = stmt.getUpdateCount();
+                if( updateCount == 1 ){
+                    System.out.println( "1 world inserted" );
+                }else{
+                    throw new RuntimeException( "Expected 1 world to be inserted, but got " + updateCount );
+                }
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+            System.out.println("Failed to connect to db");
+        }
+    }
 }
