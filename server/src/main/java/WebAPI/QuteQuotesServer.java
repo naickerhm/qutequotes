eac60cf77770ce3c3ccae0acb97376b2970d97bf
hnaicker_cpt <hnaicker@student.wethinkcode.co.za>
2021-11-10 22:18:00 +0200

    Updated the server setup to include db setup

--- server/src/main/java/WebAPI/QuteQuotesServer.java
+++ server/src/main/java/WebAPI/QuteQuotesServer.java
@@ -1,30 +1,39 @@
 package WebAPI;
 
 import io.javalin.Javalin;
-
 import java.sql.SQLException;
-
-// import DataAccess.DatabaseConnection;
+import DataAccess.DatabaseConnection;
 
 public class QuteQuotesServer {
     private final Javalin server;
 
-    public QuteQuotesServer() {
+    public QuteQuotesServer(DatabaseConnection dbcon) {
+        
         server = Javalin.create(config -> {
             config.defaultContentType = "application/json";
             config.enableCorsForAllOrigins();
         });
 
-        this.server.get("/quotes", context -> QuoteApiHandler.getAll(context));
-        this.server.get("/quote/{id}", context -> QuoteApiHandler.getOne(context));
-        this.server.post("/quotes", context -> QuoteApiHandler.create(context));
+        this.server.get("/quotes", context -> QuoteApiHandler.getAll(context, dbcon));
+        this.server.get("/quote/{id}", context -> QuoteApiHandler.getOne(context, dbcon));
+        this.server.post("/quotes", context -> QuoteApiHandler.create(context, dbcon));
     }
 
     public static void main(String[] args) throws SQLException {
-        QuteQuotesServer server = new QuteQuotesServer();
-//        DatabaseConnection dbcon = new DatabaseConnection();
-        // dbcon.createTable();
-        server.start(5000);
+        QuteQuotesServer server;
+        DatabaseConnection dbcon = new DatabaseConnection();
+
+        boolean dbConnected = dbcon.Connect();
+
+        if(dbConnected) {
+            dbcon.createTable();
+            server = new QuteQuotesServer(dbcon);
+            server.start(5000);
+        } else {
+            System.out.println("DB Connection failed");
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> dbcon.Close()));
     }
 
     public void start(int port) {
